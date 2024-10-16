fd9e2575f367f39f37468e66abb3a247f37e8d63
Emmett Reibel <Re4379Em0427@pusd.us>
2024-10-16 09:55:51 -0700

    keybinds, updated branch

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -9,17 +9,17 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionx = Input.get_axis("ui_left", "ui_right")
+	var directionx = Input.get_axis("move_left", "move_right")
 #	print(directionx)
-	var directiony = Input.get_axis("ui_down", "ui_up")
+	var directiony = Input.get_axis("move_down", "move_up")
 	velocity.x = directionx * SPEED
 	velocity.y = directiony * -SPEED
-	if Input.is_action_just_pressed("ui_left") == true:
+	if Input.is_action_just_pressed("move_left") == true:
 		tedk.set_frame(3)
-	elif Input.is_action_just_pressed("ui_right") == true:
+	elif Input.is_action_just_pressed("move_right") == true:
 		tedk.set_frame(2)
-	elif Input.is_action_just_pressed("ui_up") == true:
+	elif Input.is_action_just_pressed("move_up") == true:
 		tedk.set_frame(1)
-	elif Input.is_action_just_pressed("ui_down") == true:
+	elif Input.is_action_just_pressed("move_down") == true:
 		tedk.set_frame(0)
 	move_and_slide()
